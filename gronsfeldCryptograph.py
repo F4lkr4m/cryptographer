65354cfbcfe77e7e2bddc0084a7a83b49ad7ae5b
falkr4m <falkr4m@yandex.ru>
2019-07-18 14:43:32 +0300

    added gronsfeldCryptograph class

---
+++ gronsfeldCryptograph.py
@@ -0,0 +1,36 @@
+from cryptographer import Cryptographer
+
+
+class GronsfeldCryptograph(Cryptographer):
+
+    @staticmethod
+    def encrypt(text, key):
+        cryptoText = ''
+        key = str(key)
+        for i in range(len(text)):
+            if text[i] not in Cryptographer.RUS_ALP:
+                cryptoText += text[i]
+            else:
+                realShift = int(key[i % len(key)])  # polyalphabet shift
+                if Cryptographer.RUS_ALP.index(text[i]) + realShift > len(Cryptographer.RUS_ALP) - 1:
+                    newInd = Cryptographer.RUS_ALP.index(text[i]) + realShift - len(Cryptographer.RUS_ALP)
+                else:
+                    newInd = Cryptographer.RUS_ALP.index(text[i]) + realShift
+                cryptoText += Cryptographer.RUS_ALP[newInd]
+        return cryptoText
+
+    @staticmethod
+    def decrypt(cryptoText, key):
+        deCryptoText = ''
+        key = str(key)
+        for i in range(len(cryptoText)):
+            if cryptoText[i] not in Cryptographer.RUS_ALP:
+                deCryptoText += cryptoText[i]
+            else:
+                realShift = int(key[i % len(key)])
+                if Cryptographer.RUS_ALP.index(cryptoText[i]) - realShift < 0:
+                    newInd = len(Cryptographer.RUS_ALP) + (Cryptographer.RUS_ALP.index(cryptoText[i]) - realShift)
+                else:
+                    newInd = Cryptographer.RUS_ALP.index(cryptoText[i]) - realShift
+                deCryptoText += Cryptographer.RUS_ALP[newInd]
+        return deCryptoText
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from caesarCryptograph import CaesarCryptograph
+from gronsfeldCryptograph import GronsfeldCryptograph
 
 s = 'встреча состоится у школы'
 x = CaesarCryptograph.encrypt(s, 3)
 print(x)
 print(CaesarCryptograph.decrypt(x, 3))
+m = GronsfeldCryptograph.encrypt(s, 1213)
+print(m)
+print(GronsfeldCryptograph.decrypt(m, 1213))
